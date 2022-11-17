7ddd7880c5fa2acf25bf385233d6777c1b1bbb3d
Benito Kestelman <bkestelman@outlook.com>
2022-11-16 23:00:36 -0500

    Initialize features with starting positions for each piece type

--- chess/state.py
+++ chess/state.py
@@ -3,25 +3,56 @@ class State:
         self.feature_planes = [None, None, None]
         WHITE = 0, BLACK = 1, MISC = 2
         self.feature_planes[WHITE] = {
-                'P': np.zeros(8, 8),
-                'R': np.zeros(8, 8),
-                'N': np.zeros(8, 8),
-                'B': np.zeros(8, 8),
-                'Q': np.zeros(8, 8),
-                'K': np.zeros(8, 8),
+                'P': init_pawns(),
+                'R': init_rooks(),
+                'N': init_knights(),
+                'B': init_bishops(),
+                'Q': init_queen(),
+                'K': init_king(),
                 'castling_allowed': np.ones(1),
                 }
         self.feature_planes[BLACK] = {
-                'P': np.zeros(8, 8),
-                'R': np.zeros(8, 8),
-                'N': np.zeros(8, 8),
-                'B': np.zeros(8, 8),
-                'Q': np.zeros(8, 8),
-                'K': np.zeros(8, 8),
+                'P': init_pawns(),
+                'R': init_rooks(),
+                'N': init_knights(),
+                'B': init_bishops(),
+                'Q': init_queen(),
+                'K': init_king(),
                 'castling_allowed': np.ones(1)
                 }
         self.feature_planes[MISC] = {
                 'repetitions': np.zeros(1), #TODO: how should these be represented?
                 'moves_since_capture_or_pawn': np.zeros(1)
                 }
+
+def init_pawns():
+    pawns = empty_board()
+    pawns[1,:] = np.ones(8)
+    return pawns
+
+def init_rooks():
+    rooks = empty_board()
+    rooks[0,0] = 1
+    rooks[0,7] = 1
+
+def init_knights():
+    knights = empty_board()
+    knights[0,1] = 1
+    knights[0,6] = 1
+
+def init_bishops():
+    knights = empty_board()
+    knights[0,2] = 1
+    knights[0,5] = 1
+
+def init_queen():
+    knights = empty_board()
+    knights[0,3] = 1
+
+def init_king():
+    knights = empty_board()
+    knights[0,4] = 1
+
+def empty_board():
+    return np.zeros(8,8)
 
